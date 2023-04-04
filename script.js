eb9e1b0d80647a9220c85878d14aae1cf83c421f
srjfabian <josefabianmoralespaternina@gmail.com>
2023-04-04 16:27:33 -0500

    portafolio

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel').carousel({
+      interval: 2000, // tiempo de transición en milisegundos
+      pause: 'hover', // pausa el carrusel al pasar el mouse sobre él
+      wrap: true // reinicia el carrusel al llegar al final
+    });
+  });
+
+  
+  
